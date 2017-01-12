f21fbcbbe4fb214a3c9037bf619179b92b9f1783
X <986327386@qq.com>
2017-01-12 11:32:45 +0800

    searchPhotoLinks

--- 1.py
+++ 1.py
@@ -1,5 +1,14 @@
-import urllib
+from urllib import request
+from bs4 import BeautifulSoup
+import re
+webPage=request.urlopen('https://www.zhihu.com/question/38478558').read()
 
-def f():
-    print(1)
-f()
+file=open('webPage.html','wb')
+file.write(webPage)
+file.close()
+
+soup=BeautifulSoup(webPage,'html5lib')
+for link in soup.find_all('img'):
+    print(link.get('data-actualsrc'))
+
+print(soup.getText())
